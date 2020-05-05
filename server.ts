07e71a9c4f9ddd864d14e1d8ce363739028ffe5c
Santiago Martín Agra <santiagomartinagra@gmail.com>
2020-05-05 23:54:11 +0200

    chore: load port from env var

--- server.ts
+++ server.ts
@@ -1,5 +1,7 @@
 import { serve } from "https://deno.land/std/http/server.ts";
-const s = serve({ port: 8000 });
+const PORT = parseInt(Deno.env.get("PORT") || '', 10) || 8080;
+const s = serve({ port: PORT });
+console.log(`Listen on: ${PORT}`);
 
 for await (const req of s) {
   const body = JSON.stringify(await fetch('https://reqres.in/api/users?page=2').then((res) => res.json()))
