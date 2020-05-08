6ffa0adf4d9b6c723a9962e1e3a9530fed8612f4
Santiago Martín Agra <santiagomartinagra@gmail.com>
2020-05-08 20:13:13 +0200

    chore: load data from page 1

--- server.ts
+++ server.ts
@@ -1,9 +1,10 @@
 import { serve } from "https://deno.land/std/http/server.ts";
+
 const PORT = parseInt(Deno.env.get("PORT") || '', 10) || 8080;
 const s = serve(`0.0.0.0:${PORT}`);
 console.log(`Listen on: ${PORT}`);
 
 for await (const req of s) {
-  const body = JSON.stringify(await fetch('https://reqres.in/api/users?page=2').then((res) => res.json()))
+  const body = JSON.stringify(await fetch('https://reqres.in/api/users?page=1').then((res) => res.json()))
   req.respond({ body, status: 200 });
 }
